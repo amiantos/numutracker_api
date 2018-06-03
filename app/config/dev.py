89aac10c2b43088b52f7736a9d16a261f5c5d560
amiantos <bradroot@me.com>
2018-06-03 14:41:35 -0700

    add models and secret key

--- app/config/dev.py
+++ app/config/dev.py
@@ -5,3 +5,5 @@ NUMU_ENVIRONMENT = "Development"
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = "postgresql://numu@localhost:5432/numu"
+
+SECRET_KEY = "65c9cfc302b643b2d0a20d44f404314a00ce54e423ae6571"
--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import models
+
 
 app = Flask(__name__)
 
 app.config.from_object('config.dev')
 app.config.from_object('config.prod')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
@@ -14,3 +22,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
+
+

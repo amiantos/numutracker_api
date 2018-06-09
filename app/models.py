502c72948da84e1a9bf5ab9c40157c8608f54218
amiantos <bradroot@me.com>
2018-06-09 10:06:01 -0700

    updated docker-compose files for dev environment

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from main import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class NumuUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +10,9 @@ class NumuUser(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)

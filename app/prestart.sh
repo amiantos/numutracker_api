9356a8ea094a131a5f61fca5a385d0fd87dd1a70
amiantos <bradroot@me.com>
2018-06-16 10:53:56 -0700

    attempt to fix production celery tasks and test

--- app/prestart.sh
+++ app/prestart.sh
@@ -3,5 +3,5 @@
 # Run migrations
 flask db upgrade
 
-su -m celery -c "celery -A tasks beat --loglevel=info" &
+su -m celery -c "celery -A tasks beat --loglevel=info --pidfile='/tmp/celerybeat.pid'" &
 #su -m celery -c "celery -A tasks worker --loglevel=info"
--- app/tasks.py
+++ app/tasks.py
@@ -38,6 +38,12 @@ def only_one(function=None, key="", timeout=None):
 def setup_periodic_tasks(sender, **kwargs):
 
     sender.add_periodic_task(
+        5,
+        test.s('Test Celery Task'),
+        name='Test Tasks'
+    )
+
+    sender.add_periodic_task(
         900,
         test.s('Run Updates'),
         name='Updates'

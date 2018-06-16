f4c2fe5ba3b2250c823f270aefa8820748017d38
amiantos <bradroot@me.com>
2018-06-16 15:11:10 -0700

    add musicbrainz classes

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ up:
 
 test:
 	docker-compose -f docker-compose.local.yml build && docker-compose -f docker-compose.local.yml run --user=root --rm api pytest
+
+shell:
+	docker-compose -f docker-compose.local.yml build && docker-compose -f docker-compose.local.yml run --rm api bash -c "flask shell"
--- app/musicbrainz.py
+++ app/musicbrainz.py
@@ -1,7 +1,5 @@
 import musicbrainzngs as mbz
 
-from main import db
-from models import ArtistImport, Artist, Release, ReleaseType
 
 mbz.set_useragent(
     "numutracker_api",
@@ -9,9 +7,10 @@ mbz.set_useragent(
     "https://github.com/amiantos/numutracker_api",
 )
 
-def get_artist(mbid):
+
+def get_artist(artist_mbid):
     try:
-        result = mbz.get_artist_by_id(mbid)
+        result = mbz.get_artist_by_id(artist_mbid)
     except mbz.ResponseError as err:
         status = err.cause.code
         result = None
@@ -20,3 +19,39 @@ def get_artist(mbid):
         result = result["artist"]
 
     return {'status': status, 'result': result}
+
+
+def get_release(release_mbid):
+    try:
+        result = mbz.get_release_group_by_id(release_mbid)
+    except mbz.ResponseError as err:
+        status = err.cause.code
+        result = None
+    else:
+        status = 200
+        result = result["release-group"]
+
+    return {'status': status, 'result': result}
+
+
+def get_artist_releases(artist_mbid):
+    limit = 100
+    offset = 0
+    releases = []
+    page = 1
+
+    result = mbz.browse_release_groups(artist=artist_mbid, limit=limit, offset=offset)
+    releases += result['release-group-list']
+
+    if 'release-group-count' in result:
+        count = result['release-group-count']
+    
+    while len(releases) < count:
+        offset += limit
+        page += 1
+
+        result = mbz.browse_release_groups(artist=artist_mbid, limit=limit, offset=offset)
+        releases += result['release-group-list']
+    
+    return {'status': 200, 'result': releases}
+    

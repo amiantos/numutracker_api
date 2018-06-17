2ff65c6ba4d59496d44b7a4bdebed6cda6526c12
amiantos <bradroot@me.com>
2018-06-17 13:02:25 -0700

    remove migrations run command from prestart.sh

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
 
-# Run migrations
-flask db upgrade
-
 su -m celery -c "celery -A tasks beat --loglevel=info --pidfile='/tmp/celerybeat.pid'" &
 #su -m celery -c "celery -A tasks worker --loglevel=info"

f4c79a820257f603781d0ebc5d59f64725041795
Brad Root <brad@thenounproject.com>
2018-08-25 16:22:31 -0700

    import artists from v2 in batches

--- app/commands/import_artists.py
+++ app/commands/import_artists.py
@@ -8,22 +8,36 @@ from models import Artist, ArtistAka
 @app.cli.command()
 def import_artists():
     """Import artist information from API V2."""
-    print("Importing artists from V2")
-    data = grab_json("https://numutracker.com/v2/json.php?all_artists&key={}"
-                     .format(app.config.get('APIV2_KEY')))
+    limit = 200
+    offset = 0
+    total = 0
 
-    for artist in data:
-        found_artist = Artist.query.filter_by(mbid=artist['mbid']).first()
-        if found_artist is None:
-            print("ADDED: {} - {}".format(artist['name'], artist['mbid']))
-            new_artist = Artist(
-                mbid=artist['mbid'],
-                name=artist['name'],
-                sort_name=artist['sort_name'],
-                disambiguation=artist['disambiguation']
-            )
-            db.session.add(new_artist)
-            db.session.commit()
+    print("Importing artists from V2")
+
+    while True:
+        uri = "https://numutracker.com/v2/json.php?all_artists&key={}&limit={}&offset={}".format(
+            app.config.get('APIV2_KEY'), limit, offset)
+        data = grab_json(uri)
+        if not data:
+            return
+
+        for artist in data:
+            found_artist = Artist.query.filter_by(mbid=artist['mbid']).first()
+            if found_artist is None:
+                print("ADDED: {} - {}".format(artist['name'], artist['mbid']))
+                new_artist = Artist(
+                    mbid=artist['mbid'],
+                    name=artist['name'],
+                    sort_name=artist['sort_name'],
+                    disambiguation=artist['disambiguation']
+                )
+                db.session.add(new_artist)
+                db.session.commit()
+
+        offset += limit
+        total += limit
+
+        print("Processed {} Next offset: {}".format(total, offset))
 
     print("Importing artist AKA from V2")
 

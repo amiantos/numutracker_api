d5302469b2594e9352dd74f826e406e68531bd47
Brad Root <brad@thenounproject.com>
2018-08-25 15:58:46 -0700

    remove redis locking mechanism

--- app/tasks.py
+++ app/tasks.py
@@ -7,33 +7,6 @@ import backend
 
 REDIS_CLIENT = redis.Redis(host='redis', password='numu_tracker')
 
-
-def only_one(function=None, key="", timeout=None):
-    """Enforce only one celery task at a time."""
-
-    def _dec(run_func):
-        """Decorator."""
-
-        def _caller(*args, **kwargs):
-            """Caller."""
-            ret_value = None
-            have_lock = False
-            lock = REDIS_CLIENT.lock(key, timeout=timeout)
-            try:
-                have_lock = lock.acquire(blocking=False)
-                if have_lock:
-                    ret_value = run_func(*args, **kwargs)
-            finally:
-                if have_lock:
-                    lock.release()
-
-            return ret_value
-
-        return _caller
-
-    return _dec(function) if function is not None else _dec
-
-
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
 
@@ -52,7 +25,6 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 @celery.task
-@only_one(key="mb_task", timeout=60 * 60)
 def run_updates():
     numu_app.logger.info("Processing Imported Artists...")
     backend.process_imported_artists()

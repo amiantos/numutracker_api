f52e95c5f7547ee9bd37fd56d8cf06e483254f91
amiantos <bradroot@me.com>
2018-12-26 12:58:37 -0800

    fix for missing art attribute

--- backend/image_processing.py
+++ backend/image_processing.py
@@ -152,10 +152,6 @@ def scan_release_art(limit=100):
         art_success = get_release_art(release)
         if art_success:
             release.art = True
-            # Update all user releases
-            UserRelease.query.filter_by(
-                mbid=release.mbid).update(
-                    dict(art=True))
         release.date_art_check = datetime.now()
         db.session.add(release)
         db.session.commit()

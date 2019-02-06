2fe27a60a8b50cf99ea769c8005ca2a7849c3352
amiantos <bradroot@me.com>
2019-02-05 20:23:37 -0800

    convert api json output to camelCase

--- backend/serializer.py
+++ backend/serializer.py
@@ -10,7 +10,7 @@ def serializer(object, type):
     if type == "user_release":
         return user_releases(object)
     if type == "user_artist":
-        return user_artist(object)
+        return user_artist_serializer(object)
     if type == "artist_release_with_user":
         return artist_release_with_user(object)
 
@@ -21,13 +21,13 @@ def get_art_urls(object):
         for artist in object.artists:
             if artist.art is True:
                 return {
-                    "thumb": "https://numu.sfo2.digitaloceanspaces.com/artist/thumb/{}.png".format(
+                    "thumbUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/thumb/{}.png".format(
                         artist.mbid
                     ),
-                    "full": "https://numu.sfo2.digitaloceanspaces.com/artist/full/{}.png".format(
+                    "fullUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/full/{}.png".format(
                         artist.mbid
                     ),
-                    "large": "https://numu.sfo2.digitaloceanspaces.com/artist/large/{}.png".format(
+                    "largeUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/large/{}.png".format(
                         artist.mbid
                     ),
                 }
@@ -35,25 +35,25 @@ def get_art_urls(object):
         return None
     if type(object) is Release:
         return {
-            "thumb": "https://numu.sfo2.digitaloceanspaces.com/release/thumb/{}.png".format(
+            "thumbUrl": "https://numu.sfo2.digitaloceanspaces.com/release/thumb/{}.png".format(
                 object.mbid
             ),
-            "full": "https://numu.sfo2.digitaloceanspaces.com/release/full/{}.png".format(
+            "fullUrl": "https://numu.sfo2.digitaloceanspaces.com/release/full/{}.png".format(
                 object.mbid
             ),
-            "large": "https://numu.sfo2.digitaloceanspaces.com/release/large/{}.png".format(
+            "largeUrl": "https://numu.sfo2.digitaloceanspaces.com/release/large/{}.png".format(
                 object.mbid
             ),
         }
     if type(object) is Artist:
         return {
-            "thumb": "https://numu.sfo2.digitaloceanspaces.com/artist/thumb/{}.png".format(
+            "thumbUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/thumb/{}.png".format(
                 object.mbid
             ),
-            "full": "https://numu.sfo2.digitaloceanspaces.com/artist/full/{}.png".format(
+            "fullUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/full/{}.png".format(
                 object.mbid
             ),
-            "large": "https://numu.sfo2.digitaloceanspaces.com/artist/large/{}.png".format(
+            "largeUrl": "https://numu.sfo2.digitaloceanspaces.com/artist/large/{}.png".format(
                 object.mbid
             ),
         }
@@ -66,28 +66,28 @@ def artist_release_with_user(tuple):
     serialized = {
         "mbid": release.mbid,
         "title": release.title,
-        "artist_names": release.artist_names,
+        "artistNames": release.artist_names,
         "type": release.type,
         "art": get_art_urls(release),
-        "date_release": release.date_release,
-        "date_added": release.date_added,
-        "date_updated": release.date_updated,
+        "dateRelease": release.date_release,
+        "dateAdded": release.date_added,
+        "dateUpdated": release.date_updated,
         "links": {
-            "apple_music": release.apple_music_link,
+            "appleMusic": release.apple_music_link,
             "spotify": release.spotify_link,
         },
         "artists": [artist(x) for x in release.artists],
-        "user_data": {},
+        "userData": {},
     }
     if user_release:
-        serialized["user_data"] = {
+        serialized["userData"] = {
             "uuid": user_release.uuid,
             "listened": user_release.listened,
-            "date_listened": user_release.date_listened,
-            "date_added": user_release.date_added,
-            "date_updated": user_release.date_updated,
-            "user_artist_uuids": [
-                user_artist.uuid for user_artist in user_release.user_artists
+            "dateListened": user_release.date_listened,
+            "dateAdded": user_release.date_added,
+            "dateUpdated": user_release.date_updated,
+            "userArtists": [
+                user_artist_serializer(user_artist) for user_artist in user_release.user_artists
             ],
         }
     return serialized
@@ -97,32 +97,32 @@ def user_releases(user_release):
     serialized = {
         "mbid": user_release.mbid,
         "title": user_release.title,
-        "artist_names": user_release.artist_names,
+        "artistNames": user_release.artist_names,
         "type": user_release.type,
         "art": get_art_urls(user_release.release),
-        "date_release": user_release.date_release,
-        "date_added": user_release.date_added,
-        "date_updated": user_release.release.date_updated,
+        "dateRelease": user_release.date_release,
+        "dateAdded": user_release.date_added,
+        "dateUpdated": user_release.release.date_updated,
         "links": {
-            "apple_music": user_release.apple_music_link,
+            "appleMusic": user_release.apple_music_link,
             "spotify": user_release.spotify_link,
         },
         "artists": [artist(x) for x in user_release.release.artists],
-        "user_data": {
+        "userData": {
             "uuid": user_release.uuid,
             "listened": user_release.listened,
-            "date_listened": user_release.date_listened,
-            "date_added": user_release.date_added,
-            "date_updated": user_release.date_updated,
-            "user_artist_uuids": [
-                user_artist.uuid for user_artist in user_release.user_artists
+            "dateListened": user_release.date_listened,
+            "dateAdded": user_release.date_added,
+            "dateUpdated": user_release.date_updated,
+            "userArtists": [
+                user_artist_serializer(user_artist) for user_artist in user_release.user_artists
             ],
         },
     }
     return serialized
 
 
-def user_artist(user_artist):
+def user_artist_serializer(user_artist):
     total_releases = Release.query.filter(
         Release.mbid.in_(
             db.session.query(ArtistRelease.release_mbid)
@@ -157,18 +157,18 @@ def user_artist(user_artist):
     serialized = {
         "mbid": user_artist.mbid,
         "name": user_artist.artist.name,
-        "sort_name": user_artist.artist.sort_name,
+        "sortName": user_artist.artist.sort_name,
         "disambiguation": user_artist.artist.disambiguation,
         "art": get_art_urls(user_artist.artist),
-        "date_updated": user_artist.artist.date_updated,
-        "recent_release_date": recent_release_date,
-        "user_data": {
+        "dateUpdated": user_artist.artist.date_updated,
+        "recentReleaseDate": recent_release_date,
+        "userData": {
             "uuid": user_artist.uuid,
             "following": user_artist.following,
-            "date_followed": user_artist.date_followed,
-            "date_updated": user_artist.date_updated,
-            "total_releases": total_releases,
-            "listened_releases": listened_releases,
+            "dateFollowed": user_artist.date_followed,
+            "dateUpdated": user_artist.date_updated,
+            "totalReleases": total_releases,
+            "listenedReleases": listened_releases,
         },
     }
     return serialized
@@ -178,8 +178,8 @@ def artist(artist):
     return {
         "mbid": artist.mbid,
         "name": artist.name,
-        "sort_name": artist.sort_name,
+        "sortName": artist.sort_name,
         "disambiguation": artist.disambiguation,
         "art": get_art_urls(artist),
-        "date_updated": artist.date_updated,
+        "dateUpdated": artist.date_updated,
     }

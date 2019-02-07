3647312a2f8509bf5f71b20b7ec4e41135ab459a
Brad Root <brad@thenounproject.com>
2019-02-06 16:44:49 -0800

    remove server_clock from responses

--- response.py
+++ response.py
@@ -4,16 +4,11 @@ from flask import jsonify
 
 
 def success(result=None):
-    return jsonify(success=True, result=result, server_clock=int(time.time())), 200
+    return jsonify(success=True, result=result), 200
 
 
 def error(message=None):
-    return (
-        jsonify(
-            success=False, result={"message": message}, server_clock=int(time.time())
-        ),
-        400,
-    )
+    return jsonify(success=False, result={"message": message}), 400
 
 
 def unauthorized():

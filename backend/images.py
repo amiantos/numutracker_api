56c0c919f3979d21947ae140234c116cf1b4bb65
Brad Root <brad@thenounproject.com>
2019-02-14 17:44:33 -0800

    update images module to remove user_release updates

--- backend/images.py
+++ backend/images.py
@@ -139,7 +139,7 @@ def scan_artist_art(limit=100):
 
         artist.date_art_check = datetime.now()
         db.session.add(artist)
-        db.session.commit()
+    db.session.commit()
 
     return True
 
@@ -165,10 +165,8 @@ def scan_release_art(limit=100):
         art_success = get_release_art(release)
         if art_success:
             release.art = True
-            # Update all user releases
-            UserRelease.query.filter_by(mbid=release.mbid).update(dict(art=True))
         release.date_art_check = datetime.now()
         db.session.add(release)
-        db.session.commit()
+    db.session.commit()
 
     return True
